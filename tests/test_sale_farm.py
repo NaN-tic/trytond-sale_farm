9ab358501b8b5f4d522c876146f6ef3cb21537b1
Jes?s Mart?n Jim?nez <jesus@nan-tic.com>
2016-08-16 10:27:13 +0200

    Upgrade test

--- tests/test_sale_farm.py
+++ tests/test_sale_farm.py
@@ -4,23 +4,13 @@ import doctest
 import unittest
 
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_setup, doctest_teardown
 
 
-class TestCase(unittest.TestCase):
+class TestCase(ModuleTestCase):
     'Test module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('sale_farm')
-
-    def test0005views(self):
-        'Test views'
-        test_view('sale_farm')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'sale_farm'
 
 
 def suite():
